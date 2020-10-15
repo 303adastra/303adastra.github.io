6b9d9b616ae8d87b625b312cc3a21fb895e77bec
Sean Marquez <capsulecorplab@gmail.com>
2020-10-15 02:12:40 -0700

    update build step w/ custom docker image

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-export PATH="$PWD/bin:$PATH"
-hugo --minify -v
+if [ ! -r ./node_modules ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml bash -c "npm ci"
+fi
+if [ ! -r ./public ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml bash -c "hugo --minify -v --destination public"
+fi
