8fa03ef615637789d292f2f4c7faeae3f8eb4464
Sean Marquez <capsulecorplab@gmail.com>
2020-10-26 16:20:34 -0700

    update docker image to use alpine & patch lodash
    - update docker image to use capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine in build.sh & serve.sh
    - run `npm audit fix` to patch security vulnerability in lodash
    - remove bin/asciidoctor

--- serve.sh
+++ serve.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-export PATH="$PWD/bin:$PATH"
-hugo --minify && hugo serve --bind=0.0.0.0 --buildDrafts --disableFastRender
+docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine 'hugo serve --bind=0.0.0.0 --buildDrafts --disableFastRender --destination public'
