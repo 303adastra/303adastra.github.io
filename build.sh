0f2435e406fee7645dd46790e73d039a63342330
Sean Marquez <capsulecorplab@gmail.com>
2021-04-11 23:43:54 -0700

    add --buildDrafts flag to hugo build command

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -r ./node_modules ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'npm ci'
-fi
 if [ ! -r ./public ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'hugo --minify -v --destination public'
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'hugo --minify -v --buildDrafts --destination public'
 fi
